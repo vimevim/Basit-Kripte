6db9c060924281b9c8d004958342606b741e5fe7
Hasan Sezer Taşan <vimevim@users.noreply.github.com>
2020-10-21 05:12:25 +0300

    encode ve decode, random key
    Decode ve Encode metodları kısaltıldı, alpha değişkeni güçlendirildi ve random key üretme eklendi.

--- Cryptography.py
+++ Cryptography.py
@@ -1,47 +1,27 @@
-alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ "
-key ="XPMGTDHLYONZBWEARKJUFSCIQV,"
-keepGoing = True
-
+import string
+import random
+lettersDigitsPanctuations = list(string.ascii_letters + string.digits + string.punctuation+ " ") #sorted alphabet, symbolls, digits, punctuations
+alpha =''.join(lettersDigitsPanctuations)#declare list to the string without seperations
+random.shuffle(lettersDigitsPanctuations)#shuffle alphabet, symbolls, digits, punctuations
+key=''.join(lettersDigitsPanctuations)#declare shuffled list to the string without seperations
+keepGoing = True#loop forever
 def encode(encrypted):# Parametre
     encrypted = encrypted.strip()
-    encrypted = encrypted.upper()
     crypted = ""
-    alphaKeyIndex = 0  # alpha ve key için harf basamağı
-    cryptedEncryptedIndex = 0  # crypted ve encrypted için harf basamağı
-    while alphaKeyIndex < len(key):  # alpha ve key basamağı tamamlama döngüsü
-        while cryptedEncryptedIndex < len(encrypted):  # şifrelenecek basamağı tamamlama döngüsü
-            if encrypted[cryptedEncryptedIndex] in alpha[
-                alphaKeyIndex]:  # eğer encrypted'ın indeksi alpha'nın indexi ile çakışırsa
-                crypted += key[alphaKeyIndex]  # çakışma olduğu durumda gerçekleşen, crypted değişkenini düzenleyen kod
-                if(len(encrypted)==len(crypted)):
-                    return "Encode edilmiş şifreniz: " + crypted
-                #print(crypted)  # her harf eklendiğinde ekrana crypted değişkenini yazdır
-                cryptedEncryptedIndex += 1  # her harf eklendiğinde  encrypted harf basamağını bir arttır
-                alphaKeyIndex = 0  # harf eklendiğinde alpha ve key harf basamağını sıfırla
-            alphaKeyIndex += 1  # encrypted'ın indeksi alpha'nın indexi ile çakışsa da çakışmasa da alpha ve key harf basamağını arttır
+    for i in encrypted:#encrypted boyunca dön
+        crypted += str(key[key.find(key[alpha.find(i)])])#burası iyi bir açıklamayı hak ediyor
+    return "Encode edilmiş şifreniz: " + crypted
 
 def decode(crypted):  # Parametre
     crypted = crypted.strip()
-    crypted = crypted.upper()
     encrypted = ""
-    alphaKeyIndex = 0  # alpha ve key için harf basamağı
-    cryptedEncryptedIndex = 0  # crypted ve encrypted için harf basamağı
-    while alphaKeyIndex < len(alpha):  # alpha ve key basamağı tamamlama döngüsü
-        while cryptedEncryptedIndex < len(crypted):  # şifrelenecek basamağı tamamlama döngüsü
-            if crypted[cryptedEncryptedIndex] in key[
-                alphaKeyIndex]:  # eğer encrypted'ın indeksi alpha'nın indexi ile çakışırsa
-                encrypted += alpha[alphaKeyIndex]  # çakışma olduğu durumda gerçekleşen, crypted değişkenini düzenleyen kod
-                if (len(encrypted) == len(crypted)):
-                    return "Decode edilmiş şifreniz: " + encrypted
-                #print(encrypted)  # her harf eklendiğinde ekrana crypted değişkenini yazdır
-                cryptedEncryptedIndex += 1  # her harf eklendiğinde  encrypted harf basamağını bir arttır
-                alphaKeyIndex = 0  # harf eklendiğinde alpha ve key harf basamağını sıfırla
-            alphaKeyIndex += 1  # encrypted'ın indeksi alpha'nın indexi ile çakışsa da çakışmasa da alpha ve key harf basamağını arttır
+    for a in crypted:
+        encrypted += str(alpha[alpha.find(alpha[key.find(a)])])
+    return "Decode edilmiş şifreniz: " + encrypted
 
 def menu():
     print("---MENU---\n1 - Encode.\n2 - Decode.\n3 - Mesaj.")
     response = input("Menü indeksi giriniz: ")
-
     if response == "1":
         plain = input("Encode yapılacak kelimeyi gir: ")
         print(encode(plain))
@@ -55,5 +35,4 @@ def menu():
 def main():
     while (keepGoing):
         menu()
-
 main()
