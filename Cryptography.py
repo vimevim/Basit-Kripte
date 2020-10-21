290c5e2672b53d4cf64c27ee39df1d49f11a87eb
Hasan Sezer Taşan <vimevim@users.noreply.github.com>
2020-10-21 06:24:16 +0300

    strip kodları
    strip kodlarını taşıdım ve satır azaltması sağladım

--- Cryptography.py
+++ Cryptography.py
@@ -1,21 +1,18 @@
 import string
 import random
-#random key generator
 lettersDigitsPanctuations = list(string.ascii_letters + string.digits + string.punctuation+ " ") #sorted alphabet, symbolls, digits, punctuations
 alpha =''.join(lettersDigitsPanctuations)#declare list to the string without seperations
 random.shuffle(lettersDigitsPanctuations)#shuffle alphabet, symbolls, digits, punctuations
 key=''.join(lettersDigitsPanctuations)#declare shuffled list to the string without seperations
-#random key generator
 keepGoing = True#loop forever
-def encode(encrypted):# encode method
+def encode(encrypted):# Parametre
     encrypted = encrypted.strip()
     crypted = ""
-    for i in encrypted:#loop while encrypted
-        crypted += str(key[key.find(key[alpha.find(i)])])#this line needs a good explanation 
+    for i in encrypted:#encrypted boyunca dön
+        crypted += str(key[key.find(key[alpha.find(i)])])#burası iyi bir açıklamayı hak ediyor
     return "Encode edilmiş şifreniz: " + crypted
 
-def decode(crypted):#decode method
-    crypted = crypted.strip()
+def decode(crypted):  # Parametre
     encrypted = ""
     for a in crypted:
         encrypted += str(alpha[alpha.find(alpha[key.find(a)])])
@@ -26,10 +23,10 @@ def menu():
     response = input("Menü indeksi giriniz: ")
     if response == "1":
         plain = input("Encode yapılacak kelimeyi gir: ")
-        print(encode(plain))
+        print(encode(plain.strip()))
     elif response == "2":
         coded = input("Decode yapılacak kelimeyi gir: ")
-        print(decode(coded))
+        print(decode(coded.strip()))
     elif response == "3":
         print("Thanks for doing secret spy stuff with me")
     else:
